c17d864f6eec481d477e23646152ba9e92a44f6d
Rodrigo Mendza Marquez <zeronotsukaima4@gmail.com>
2020-11-29 14:16:56 -0600

    ultimos cambio

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ conectarDB();
 
 // Opciones de cors
 const opcionesCors = {
-    origin : process.env.FRONTURLPROD
+    origin : process.env.FRONTURL
 }
 app.use(cors(opcionesCors));
 
@@ -17,7 +17,7 @@ app.use(cors(opcionesCors));
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', process.env.FRONTURLPROD);
+    res.setHeader('Access-Control-Allow-Origin', process.env.FRONTURL);
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
