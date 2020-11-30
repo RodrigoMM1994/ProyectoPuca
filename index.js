8793f9fdd89606b2e60dfd476c61cd2e818bf8f7
Rodrigo Mendza Marquez <zeronotsukaima4@gmail.com>
2020-11-29 18:07:37 -0600

    commit bearer token

--- index.js
+++ index.js
@@ -22,13 +22,10 @@ app.use(function (req, res, next) {
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'Authorization');
-
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
-    
+
     // Pass to next layer of middleware
     next();
 });
